cb4997405e9c3d8325bc24260a63b647b8fc2d22
Gunnar Schug <Gunnar.Schug@nemo-ai.com>
2025-05-22 15:10:51 +0200

    Add initial implementation of the NEMO Library UI with login functionality and project management features

---
+++ setup.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+git pull
+conda update -n base -c defaults conda
+conda activate nemo_library_ui
+python --version
+python -m pip install --upgrade pip
+pip install --upgrade -r requirements.txt
+# pip freeze > requirements.txt 
+# pip freeze | awk -F "==" '{ print $1 }' > requirements.txt 
