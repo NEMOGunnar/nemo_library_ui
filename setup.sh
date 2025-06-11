8d909022a04f07e3e0c2f0ce4d88d280befc12c1
Gunnar Schug <Gunnar.Schug@nemo-ai.com>
2025-06-11 14:16:40 +0200

    Add PowerShell setup script for environment initialization

--- setup.sh
+++ setup.sh
@@ -5,5 +5,3 @@ conda activate nemo_library_ui
 python --version
 python -m pip install --upgrade pip
 pip install --upgrade -r requirements.txt
-# pip freeze > requirements.txt 
-# pip freeze | awk -F "==" '{ print $1 }' > requirements.txt 
