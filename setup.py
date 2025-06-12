9e04a62835ba694e57538c9bf07c987a4b147c44
Gunnar Schug <Gunnar.Schug@nemo-ai.com>
2025-06-12 11:45:25 +0200

    Remove requirements.txt and add MANIFEST.in for package data inclusion

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="nemo_library_ui",
+    version="0.1.0",
+    description="UI for the nemo_library based on FastAPI",
+    author="Gunnar Schug",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "fastapi",
+        "uvicorn",
+        "jinja2",
+        "nemo-library"
+    ],
+    entry_points={
+        "console_scripts": [
+            "nemo-ui = ui:start_ui"
+        ]
+    },
+    python_requires=">=3.11",
+)
