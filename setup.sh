8639da4dedbac9827d043cc07cf3e57718f81bbc
Gunnar Schug <Gunnar.Schug@nemo-ai.com>
2025-06-12 11:48:56 +0200

    Update setup.sh to install package in editable mode

--- setup.sh
+++ setup.sh
@@ -4,4 +4,4 @@ conda update -n base -c defaults conda
 conda activate nemo_library_ui
 python --version
 python -m pip install --upgrade pip
-pip install --upgrade -r requirements.txt
+pip install -e .
